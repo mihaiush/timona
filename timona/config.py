c0114d2a47cc4acb25fe8d59d59565e7dc0cd39b
Mihai Craiu <mihai.craiu@1and1.ro>
2023-05-28 10:25:29 +0300

    deepmerge

--- timona/config.py
+++ timona/config.py
@@ -1,6 +1,7 @@
 import requests
 import yaml
 import hashlib
+from deepmerge import always_merger
 
 CFG_FILE = 'timona.yaml'
 
@@ -31,15 +32,15 @@ def load_config(c=None):
         c = CFG_FILE
     _data = {}
 
-    def _load_config(c):
+    def _load_config(c, _data):
         f = load_config_file(c)
         f = yaml.safe_load(f)
         if 'include' in f:
             for g in f['include']:
-                _load_config(g)
+                _load_config(g, _data)
             del(f['include'])
-        _data.update(f)
-    _load_config(c)
+        _data = always_merger.merge(_data, f)  # _data.update(f)
+    _load_config(c, _data)
     h = hashlib.sha256(
         yaml.safe_dump(_data).encode('utf-8')
     ).hexdigest()  # sha256(yaml(_data))
